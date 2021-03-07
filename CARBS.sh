c00d3a7f0b08d2418544e1ca4220a1ca4446afe4
cschmitter <51250614+cschmitter@users.noreply.github.com>
2021-03-07 00:14:15 -0800

    Update CARBS.sh

--- CARBS.sh
+++ CARBS.sh
@@ -7,7 +7,7 @@ sudo apt purge -y gnome
 sudo apt install -y gnome-core
 sudo apt upgrade -y
 
-sudo apt install -y wget ripgrep firefox thunderbird gufw openssh-client topenssh-server mosh zsh kitty gvim emacs-nox texlive haskell-stack
+sudo apt install -y wget ripgrep firefox thunderbird gufw openssh-client topenssh-server mosh zsh kitty tmux gvim emacs-nox texlive haskell-stack firmware-nonfree
 
 # set up dotfiles
 cd $HOME
@@ -18,7 +18,7 @@ mkdir Pictures
 cp -n -l $HOME/dotfiles/* $HOME
 
 # install Nerd Fonts
-cd Downloads
+cd $HOME/Downloads
 git clone --depth 1 https://github.com/ryanoasis/nerd-fonts
 cd nerd-fonts
 ./install.sh "JetBrains Mono"
@@ -30,3 +30,29 @@ curl -fsSL https://pkgs.tailscale.com/stable/debian/buster.list | sudo tee /etc/
 sudo apt-get update
 sudo apt-get install -y tailscale
 sudo tailscale up
+
+# install zoom client
+cd $HOME/Downloas
+wget https://zoom.us/client/latest/zoom_amd64.deb
+sudo apt install ./zoom_amd64.deb
+
+# install oh-my-zsh
+$ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install typewritten
+git clone https://github.com/reobin/typewritten.git $ZSH_CUSTOM/themes/typewritten
+ln -s "$ZSH_CUSTOM/themes/typewritten/typewritten.zsh-theme" "$ZSH_CUSTOM/themes/typewritten.zsh-theme"
+ln -s "$ZSH_CUSTOM/themes/typewritten/async.zsh" "$ZSH_CUSTOM/themes/async"
+
+# install doom emacs
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
+doom sync
+
+# install anaconda
+cd ~/Downloads
+wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
+./Anaconda3-2020.11-Linux-x86_64.sh
+
+
+
